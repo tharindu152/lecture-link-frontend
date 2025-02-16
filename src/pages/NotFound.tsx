c4b19c3a18fa3bb288249e16feb9dd70f6c96e32
tharindu <chanaka152@gmail.com>
2025-02-16 23:15:15 +0530

    [LL-250216] Create NotFound page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,21 @@
+// src/pages/NotFound.jsx
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
+      <h1 className="text-4xl text-center md:text-6xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-center text-lg md:text-xl">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Link
+        to="/app/dashboard"
+        className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg hover:bg-blue-600 dark:bg-blue-700 dark:hover:bg-blue-800"
+      >
+        Go Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
