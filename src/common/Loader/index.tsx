34843ad09041c523c799da1aaa699a8b05a07f1a
tharindu <chanaka152@gmail.com>
2025-02-17 20:28:09 +0530

    [LL-250217] Refactor loader color

--- src/common/Loader/index.tsx
+++ src/common/Loader/index.tsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-white dark:bg-gray-900">
+    <div className="flex h-screen items-center justify-center text-grey-300 dark:bg-boxdark-2">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
