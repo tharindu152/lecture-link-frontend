ceb3d57cdd6fea3ef412106456ce57fcd8d4453b
tharindu <chanaka152@gmail.com>
2025-04-19 11:29:31 +0530

    [LL-250419] Update routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import PageTitle from '../components/Miscellaneous/PageTitle.tsx';
 import SignIn from './../pages/Authentication/SignIn';
 import SignUp from './../pages/Authentication/SignUp';
 import Dashboard from './../pages/Dashboard/Dashboard';
-import Institute from './../pages/Institute/Institute';
+import Institute from '../pages/Institute/Institute.tsx';
 import UpdateInstituteForm from '../components/Forms/UpdateInstituteForm.tsx';
 import Institutes from './../pages/Institute/Institutes';
 import Programs from './../pages/Institute/Programs';
@@ -25,6 +25,14 @@ import Qualification from '../pages/Lecturer/Qualification.tsx';
 import Lecturer from '../pages/Lecturer/Lecturer.tsx';
 import UpdateLecturerForm from '../components/Forms/UpdateLecturerForm.tsx';
 import About from '../pages/Dashboard/About.tsx';
+import PricingCard from '../pages/Lecturer/PricingCard.tsx';
+import LecturerProfile from '../pages/Lecturer/LecturerProfile.tsx';
+import InstituteProfile from '../pages/Institute/InstituteProfile.tsx';
+import AddSubjectForm from '../components/Forms/AddSubjectForm.tsx';
+import AddQualificationForm from '../components/Forms/AddQualificationForm.tsx';
+import SubjectView from '../pages/Institute/SubjectView.tsx';
+import PaymentSuccess from '../pages/Lecturer/PaymentSuccess.tsx';
+import RoleSelectionModal from '../pages/Authentication/RoleSelectionModal.tsx';
 
 export const routes: RouteObject[] = [
   { path: '/', element: <Navigate to="/auth/signin" /> },
@@ -33,6 +41,7 @@ export const routes: RouteObject[] = [
     children: [
       { path: 'signin', element: <><PageTitle title="Lecture Link | Sign In" /><SignIn /></> },
       { path: 'signup', element: <><PageTitle title="Lecture Link | Sign Up" /><SignUp /></> },
+      {path: 'role-selection', element: <><PageTitle title="Lecture Link | Role Selection" /><RoleSelectionModal /></> },
     ],
   },
   {
@@ -43,12 +52,18 @@ export const routes: RouteObject[] = [
       { path: 'dashboard', element: <><PageTitle title="Lecture Link | Dashboard" /><Dashboard /></> },
       { path: 'about', element: <><PageTitle title="Lecture Link | Dashboard" /><About /></> },
 
-      { path: 'profile', children: [
-          { index: true, element: <><PageTitle title={`Lecture Link | ${localStorage.getItem("role") === 'INSTITUTE' ? "Institute" : "Lecturer"}`} />{localStorage.getItem("role") === 'INSTITUTE' ? <Institute /> : <Lecturer/>}</> },
-          { path: 'update-institute/:instituteId', element: <><PageTitle title="Lecture Link | Update Institute" /><UpdateInstituteForm /></> },
+      { path: 'institutes', children: [
+          { index: true, element: <><PageTitle title="Institutes" /><Institutes /></> },
+          { path: ':instituteId', element: <><PageTitle title="Lecture Link | Institute" /><Institute /></> },
           { path: 'settings', element: <><PageTitle title="Lecture Link | Settings" /><UpdateInstituteForm /></> },
-          { path: 'institutes', element: <><PageTitle title="Lecture Link | Institutes" /><Institutes /></> },
-          { path: 'institutes/:instituteId', element: <><PageTitle title="Lecture Link | Institute" /><Institute /></> }
+        ]},
+
+      { path: 'profile', children: [
+          { index: true, element: <><PageTitle title={`Lecture Link | ${localStorage.getItem("role") === 'INSTITUTE' ? "Institute" : "Lecturer"}`} />{localStorage.getItem("role") === 'INSTITUTE' ? <InstituteProfile /> : <LecturerProfile/>}</> },
+          { path: 'update-institute/:instituteId', element: <><PageTitle title="Lecture Link | Update Institute" /><UpdateInstituteForm /></> },
+          { path: 'update-lecturer/:lecturerId', element: <><PageTitle title="Lecture Link | Update Lecturer" /><UpdateLecturerForm /></> },
+          { path: 'pricing-card', element:<><PageTitle title="Lecture Link | Pay" /><PricingCard /></> },
+          { path: 'sucsess', element:<><PageTitle title="Lecture Link | Payment Success" /><PaymentSuccess /></>},
         ]},
 
       { path: 'programs', children: [
@@ -60,24 +75,25 @@ export const routes: RouteObject[] = [
 
       { path: 'subjects', children: [
           { index: true, element: <><PageTitle title="Subjects" /><Subjects /></> },
-          { path: 'add-subject', element: <><PageTitle title="Lecture Link | Add Subject" /><UpdateSubjectForm /></> },
+          { path: 'add-subject', element: <><PageTitle title="Lecture Link | Add Subject" /><AddSubjectForm /></> },
           { path: 'update-subject/:subjectId', element: <><PageTitle title="Lecture Link | Update Subject" /><UpdateSubjectForm /></> },
           { path: 'filtered-subjects', element: <><PageTitle title="Lecture Link | Filtered Subjects" /><FilteredSubjects /></> },
+          { path: 'filtered-subjects/:subjectId', element: <><PageTitle title="Lecture Link | Filtered Subjects" /><SubjectView /></> },
           { path: 'smartmatch-subjects', element: <><PageTitle title="Lecture Link | Smart Match Subjects" /><SmartMatchSubjects /></> },
           { path: ':subjectId', element: <><PageTitle title="Lecture Link | Subject" /><Subject /></> }
         ]},
 
       { path: 'lecturers', children: [
           { index: true, element: <><PageTitle title="Lecturers" /><Lecturers /></> },
-          { path: 'update-lecturer/:lecturerId', element: <><PageTitle title="Lecture Link | Update Lecturer" /><UpdateLecturerForm /></> },
           { path: 'filtered-lecturers', element: <><PageTitle title="Filtered Lecturers" /><FilteredLecturers /></> },
           { path: 'smartmatch-lecturers', element: <><PageTitle title="Smart Match Lecturers" /><SmartMatchLecturers /></> },
-          { path: ':lecturerId', element: <><PageTitle title="Lecture Link | Lecturer" /><Lecturer /></> }
+          { path: ':lecturerId', element: <><PageTitle title="Lecture Link | Lecturer" /><Lecturer /></> },
+          { path: 'settings', element: <><PageTitle title="Lecture Link | Settings" /><UpdateLecturerForm /></> },
         ]},
 
       { path: 'qualifications', children: [
           { index: true, element: <><PageTitle title="Qualifications" /><Qualifications /></> },
-          { path: 'add-qualification', element: <><PageTitle title="Add Qualification" /><UpdateQualificationForm /></> },
+          { path: 'add-qualification', element: <><PageTitle title="Add Qualification" /><AddQualificationForm /></> },
           { path: 'update-qualification/:qualificationId', element: <><PageTitle title="Update Qualification" /><UpdateQualificationForm /></> },
           { path: ':qualificationId', element: <><PageTitle title="Lecture Link | Qualification" /><Qualification /></> }
         ]},
